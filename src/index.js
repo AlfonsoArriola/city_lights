ed72f3c72b980acaa4f18dc749322dbf6733da28
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-12-15 04:00:35 -0500

    watch mode added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 
-// console.log('ugh');
+console.log('hey ya');
+
 let mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWFycmlvbGEiLCJhIjoiY2pwbnB1dWh6MDVrejQzdDJnMW03NWhmYSJ9.N6VKHHE8I8vIErwvYJzcPA';
 let map = new mapboxgl.Map({
-  container: 'map',
+  container: 'base',
   style: 'mapbox://styles/mapbox/streets-v10'
 });
