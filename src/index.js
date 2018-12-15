3ca52a233373d794854bf44b79dcd7ea6415fc7e
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-12-15 02:15:49 -0500

    map box maybe

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 
 console.log('ugh');
+
+let mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWFycmlvbGEiLCJhIjoiY2pwbnB1dWh6MDVrejQzdDJnMW03NWhmYSJ9.N6VKHHE8I8vIErwvYJzcPA';
+let map = new mapboxgl.Map({
+  container: 'YOUR_CONTAINER_ELEMENT_ID',
+  style: 'mapbox://styles/mapbox/streets-v10'
+});
+
